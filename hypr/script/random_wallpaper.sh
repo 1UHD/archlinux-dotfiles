c2a5e2a2c5fe890438e2e712bb2b468b158b5b54
1UHD <kurt.haubold@t-online.de>
2025-04-04 17:20:21 +0200

    changed hyprland config layout && other shenanigans

---
+++ hypr/script/random_wallpaper.sh
@@ -0,0 +1,6 @@
+WALLPAPER_DIR="/home/kurt/.config/hypr/wallpaper"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+hyprctl hyprpaper reload ,"$WALLPAPER"
