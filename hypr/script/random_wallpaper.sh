366dddf45c4ba602e45b85169ac447e001e60269
1UHD <kurt.haubold@t-online.de>
2025-04-04 18:04:44 +0200

    minor changes

--- hypr/script/random_wallpaper.sh
+++ hypr/script/random_wallpaper.sh
@@ -4,3 +4,4 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 hyprctl hyprpaper reload ,"$WALLPAPER"
+notify-send "Hyprpaper" "Changed wallpaper to $WALLPAPER" --icon image
